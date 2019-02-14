d0c2d03e86e7476fecfe1d746fb86a8c20ef25b9
tmaiagq <tmaiagq@gmail.com>
2019-02-14 20:08:22 -0200

    Inclusão da sessão vendas

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-setTimeout(function(){
-	$("#cadastro_modal").modal("show");
-}, 1000)
+// setTimeout(function(){
+// 	$("#cadastro_modal").modal("show");
+// }, 1000)
 // setTimeout(function(){
 // 	$("#cadastro_modal").modal("hide");
 // }, 5000)
+$('#vendas_collapse').on('show.bs.collapse', function () {
+	$('a[href="#vendas_collapse"]').text("Ver menos")
+})
+$('#vendas_collapse').on('hidden.bs.collapse', function () {
+	$('a[href="#vendas_collapse"]').text("Ver mais")
+})
